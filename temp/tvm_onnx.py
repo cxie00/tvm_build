c3e6199015465579da00372bb2030443a12d5821
Chloe Xie <t-xiechloe@microsoft.com>
2022-07-20 10:28:18 -0700

    Changed input_name

--- temp/tvm_onnx.py
+++ temp/tvm_onnx.py
@@ -7,11 +7,12 @@ import mlflow
 
 # Load a previously converted model. This model was converted from skl to ONNX with Hummingbird.
 onnx_model = mlflow.onnx.load_model("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
-ml_model = mlflow.onnx.Model.load("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
+ml_model = mlflow.models.Model.load("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
 input_example =  ml_model.load_input_example("0/b81d20537a9345cf9126d7c8bdc7a2c6/artifacts/onnx_model")
-input_shape = input_example.shape
-print(f'input_shape: {input_shape}')
-print(f'input_shape type: {type(input_shape)}')
+
+x = np.array(input_example)[np.newaxis, np.newaxis, :, :]
+input_shape = x.shape
+
 # Code below is from TVM ONNX example in their official docs 
 # https://tvm.apache.org/docs/how_to/compile_models/from_onnx.html#compile-the-model-with-relay
 target = "llvm"
@@ -25,3 +26,6 @@ with tvm.transform.PassContext(opt_level=1):
     executor = relay.build_module.create_executor(
         "graph", mod, tvm.cpu(0), target, params
     ).evaluate()
+
+dtype = "float32"
+tvm_output = executor(tvm.nd.array(x.astype(dtype))).numpy()
